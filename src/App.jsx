2be52b9968c8b9922ae7813fe925fd944ba32ff2
Avinash sharma <sharmaavi2196@gmail.com>
2025-02-03 08:45:04 +0530

    Add Body component and refactor Header; replace old Header implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Body from "./Body";
+import { useEffect, useReducer } from "react";
 
+const initialState = {
+  questions: [],
+
+  // "loading","error","ready","active","finished"
+  status: "loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("action type unknown");
+  }
+};
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await fetch(`http://localhost:8000/questions`);
+        if (!response.ok) {
+          throw new Error("failed to fetch questions");
+        }
+        const data = await response.json();
+        dispatch({ type: "dataReceived", payload: data });
+        console.log(data);
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+        console.error(err);
+      }
+    };
+    fetchQuestions();
+  }, []);
   return (
     <div>
-      <DateCounter />
+      <Header />
+
+      <Body>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Body>
     </div>
   );
 }
