195ae2acf47e02cc9c3384fa41e4d2819604dc78
Avinash sharma <sharmaavi2196@gmail.com>
2025-02-04 07:59:49 +0530

    Refactor App component to include Loader, Error, and StartScreen; remove old Loader and Error implementations

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Header from "./Header";
 import Body from "./Body";
 import { useEffect, useReducer } from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -20,7 +23,9 @@ const reducer = (state, action) => {
   }
 };
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
@@ -43,8 +48,9 @@ function App() {
       <Header />
 
       <Body>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Body>
     </div>
   );
