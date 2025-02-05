c8c2ae82881f9cc6654312fbf1205255cedeb9f0
Avinash sharma <sharmaavi2196@gmail.com>
2025-02-05 18:19:02 +0530

    Add Progress component to display question index and scoring; update App component to integrate Progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Questions from "./Questions";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
@@ -14,6 +15,7 @@ const initialState = {
   status: "loading",
   index: 0,
   answer: null,
+  points: 0,
 };
 
 const reducer = (state, action) => {
@@ -42,12 +44,16 @@ const reducer = (state, action) => {
   }
 };
 function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
@@ -77,6 +83,13 @@ function App() {
         )}
         {status === "active" && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              maxPossiblePoints={maxPossiblePoints}
+              points={points}
+              answer={answer}
+            />
             <Questions
               question={questions[index]}
               answer={answer}
